14a140263a5beeae128e65cfeb316a92079ce4b8
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-04 20:21:52 +0000

    Get the status code, headers, and body from the triplet returned by app.call

--- http_server.rb
+++ http_server.rb
@@ -10,6 +10,9 @@ while session = server.accept
   request = session.gets
   puts request
 
+  # 1
+  status, headers, body = app.call({})
+  
   session.print "HTTP/1.1 200\r\n" # 1
   session.print "Content-Type: text/html\r\n" # 2
   session.print "\r\n" # 3
