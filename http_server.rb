0593581d2a9b0bcb695a24318b0e6d1e1c6c1cd9
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-04 20:22:41 +0000

    Use the status code to build the status line

--- http_server.rb
+++ http_server.rb
@@ -12,8 +12,8 @@ while session = server.accept
 
   # 1
   status, headers, body = app.call({})
-  
-  session.print "HTTP/1.1 200\r\n" # 1
+  # 2
+  session.print "HTTP/1.1 #{status}\r\n"
   session.print "Content-Type: text/html\r\n" # 2
   session.print "\r\n" # 3
   session.print "Hello world! The time is #{Time.now}" #4
